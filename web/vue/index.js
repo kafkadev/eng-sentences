0481332cb9c01045753bd0ece8d65a6c73bbc0ff
kafkadev <kafkadeveloper@gmail.com>
2020-10-26 11:09:55 +0300

    add json parse

--- web/vue/index.js
+++ web/vue/index.js
@@ -1,5 +1,3 @@
-
-
 var Main = httpVueLoader('./vue/main.vue')
 var User = httpVueLoader('./vue/user.vue')
 var Sidebar = httpVueLoader('./vue/sidebar.vue')
@@ -27,89 +25,88 @@ var NamedWrapper = httpVueLoader('./vue/named.vue')
 
 
 /* Router and App setup: */
-var routes = [
-{
-  path: '/post/:post_id',
-  name: 'posts',
-  props: true,
-  component: Post
-},
-{
-  path: '/',
-  name: 'home',
-  props: true,
-  component: SourceNews
-},{
-  path: '/SourceSubtitle',
-  name: 'SourceSubtitle',
-  props: true,
-  component: SourceSubtitle
-},
-{
-  path: '/search',
-  name: 'SearchWord',
-  component: SearchWord
-},
-{
-  path: '/search-outsource',
-  name: 'SearchWordOutsource',
-  component: SearchWordOutsource
-},
-{
-  path: '/source_dictionary',
-  name: 'SourceDictionary',
-  component: SourceDictionary
-},
-{
-  path: '/translate-link',
-  name: 'TranslateLink',
-  component: TranslateLink
-},
-{
-  path: '/translate-subtitle',
-  name: 'TranslateSubtitle',
-  component: TranslateSubtitle
-},
-{
-  path: '/translate-raw',
-  name: 'TranslateRaw',
-  component: TranslateRaw
-},
-{
-  path: '/source-news',
-  name: 'SourceNews',
-  component: SourceNews
-},
-{
-  path: '/source-articles',
-  name: 'SourceArticles',
-  component: SourceArticles
-},
-{
-  path: '/source-files',
-  name: 'SourceFiles',
-  component: SourceFiles
-},
-{
-  path: '/source-video',
-  name: 'SourceVideo',
-  component: SourceVideo
-},
-{
-  path: '/source_favorite_words',
-  name: 'SourceFavoriteWords',
-  component: SourceFavoriteWords
-},
-{
-  path: '/source_favorite_sentences',
-  name: 'SourceFavoriteSentences',
-  component: SourceFavoriteSentences
-}
+var routes = [{
+        path: '/post/:post_id',
+        name: 'posts',
+        props: true,
+        component: Post
+    },
+    {
+        path: '/',
+        name: 'home',
+        props: true,
+        component: SourceNews
+    }, {
+        path: '/SourceSubtitle',
+        name: 'SourceSubtitle',
+        props: true,
+        component: SourceSubtitle
+    },
+    {
+        path: '/search',
+        name: 'SearchWord',
+        component: SearchWord
+    },
+    {
+        path: '/search-outsource',
+        name: 'SearchWordOutsource',
+        component: SearchWordOutsource
+    },
+    {
+        path: '/source_dictionary',
+        name: 'SourceDictionary',
+        component: SourceDictionary
+    },
+    {
+        path: '/translate-link',
+        name: 'TranslateLink',
+        component: TranslateLink
+    },
+    {
+        path: '/translate-subtitle',
+        name: 'TranslateSubtitle',
+        component: TranslateSubtitle
+    },
+    {
+        path: '/translate-raw',
+        name: 'TranslateRaw',
+        component: TranslateRaw
+    },
+    {
+        path: '/source-news',
+        name: 'SourceNews',
+        component: SourceNews
+    },
+    {
+        path: '/source-articles',
+        name: 'SourceArticles',
+        component: SourceArticles
+    },
+    {
+        path: '/source-files',
+        name: 'SourceFiles',
+        component: SourceFiles
+    },
+    {
+        path: '/source-video',
+        name: 'SourceVideo',
+        component: SourceVideo
+    },
+    {
+        path: '/source_favorite_words',
+        name: 'SourceFavoriteWords',
+        component: SourceFavoriteWords
+    },
+    {
+        path: '/source_favorite_sentences',
+        name: 'SourceFavoriteSentences',
+        component: SourceFavoriteSentences
+    }
 ];
 
 var router = new VueRouter({
-  //mode: 'history',
-  routes: routes
+    //mode: 'history',
+    routes: routes
 });
 
 
@@ -117,68 +114,68 @@ setTimeout(() => {
 
 
 
-},0)
+}, 0)
 
 Vue.mixin({
-  components: {
-    Sidebar,
-    SentencesTable,
-    WordButtons,
-    NavList
-  }
+    components: {
+        Sidebar,
+        SentencesTable,
+        WordButtons,
+        NavList
+    }
 })
 
 const appsd = new Vue({
 
-  data() {
-    return {
-      apiUrl : '/api',
-      linkText : 'https://css-tricks.com/clean-up-your-css/',
-      articleText : '',
-      highlightText : '',
-      articleForm : '',
-      sidebarAddWordText : '',
-      saveWordHistoryCollection:[],
-      saveWordHistoryCollection1:[],
-      articleTextArr:[],
-      sourceLinks:[],
-      tableHelperWords: [],
-      dictionaryData: [],
-      leftTable: {
-        'getSearch':[],
-        'getArticles':[],
-        'getVideos':[],
-        'getWords':[],
-        'getDict':[],
-        'getDictFamily':[],
-      },
-      showDictionary: true,
-    }
-  },
-  router: router,
-  el: '#app',
-  render: h => h(Main),
-  template: '<Main/>',
-  methods: {
-    searchWord(text = '', min_size = 20) {
-      text = text.trim().toLowerCase();
-      if (text) {
-        this.saveWordHistoryCollection1.push(text)
+    data() {
+        return {
+            apiUrl: '/api',
+            linkText: 'https://css-tricks.com/clean-up-your-css/',
+            articleText: '',
+            highlightText: '',
+            articleForm: '',
+            sidebarAddWordText: '',
+            saveWordHistoryCollection: [],
+            saveWordHistoryCollection1: [],
+            articleTextArr: [],
+            sourceLinks: [],
+            tableHelperWords: [],
+            dictionaryData: [],
+            leftTable: {
+                'getSearch': [],
+                'getArticles': [],
+                'getVideos': [],
+                'getWords': [],
+                'getDict': [],
+                'getDictFamily': [],
+            },
+            showDictionary: true,
+        }
+    },
+    router: router,
+    el: '#app',
+    render: h => h(Main),
+    template: '<Main/>',
+    methods: {
+        searchWord(text = '', min_size = 20) {
+            text = text.trim().toLowerCase();
+            if (text) {
+                this.saveWordHistoryCollection1.push(text)
 
-        this.leftTable = []
-        this.tableHelperWords = []
+                this.leftTable = []
+                this.tableHelperWords = []
 
 
-        setTimeout(() => {
-          window.scrollTo(0, 0)
-          fetch(this.$root.apiUrl + '/getSentences?limit=100&text=' + text + '&min_size=' + min_size).then((response) => {
-            return response.json()
-          }).then((data) => {
-            this.leftTable = data
+                setTimeout(() => {
+                    window.scrollTo(0, 0)
+                    fetch(this.$root.apiUrl + '/getSentences?limit=100&text=' + text + '&min_size=' + min_size).then((response) => {
+                        return response.json()
+                    }).then((data) => {
+                        this.leftTable = data
 
-          })
+                    })
 
-         /* if (text.split(' ').length && text.split(' ').length < 2) {
+                    /* if (text.split(' ').length && text.split(' ').length < 2) {
 
             fetch(this.$root.apiUrl + '/word_outsource.php?text=' + text + '&type=word&url=0').then((response) => {
               return response.json()
@@ -196,132 +193,131 @@ const appsd = new Vue({
 
         } */
 
-      },100)
-      }
+                }, 100)
+            }
 
+        },
+        textSelectorEvent(text) {
+
+
+            if (text.trim() && typeof text.trim() === 'string' && !JSON.parse(text.trim())) {
+                console.log(text.trim());
+                this.tableHelperWords = []
+
+                //this.showDictionary = false
+                //text = text.trim().replace(/([^a-z\s']+)/gi, '');
+                this.sidebarAddWordText = text
+                this.highlightText = text
+                this.searchWord(text)
+
+
+            }
+            //this.sidebarAddWordText = window.getSelection().toString()
+            //this.doubleClick(window.getSelection().toString())
+        },
+        textSelector(e) {
+            e.preventDefault();
+
+            var text = window.getSelection().toString()
+            if (text.trim()) {
+                this.tableHelperWords = []
+
+                //this.showDictionary = false
+                //text = text.trim().replace(/([^a-z\s']+)/gi, '');
+                this.sidebarAddWordText = text
+                this.highlightText = text
+                this.searchWord(text)
+
+
+            }
+            //this.sidebarAddWordText = window.getSelection().toString()
+            //this.doubleClick(window.getSelection().toString())
+        },
+        doubleClick(text) {
+            this.tableHelperWords = []
+            this.dictionaryData = []
+                //this.showDictionary = false
+            text = text.trim().replace(/([^a-z]+)/gi, '');
+            this.sidebarAddWordText = text
+            this.highlightText = text
+            this.searchWord(text)
+                /*fetch('http://sentences.test/api/dictionary.php?type=important&text='+text).then((response) => {
+                  return response.json()
+                }).then((data) => {
+                 // console.log(data);
+                  this.dictionaryData = data
+                })*/
+
+
+        },
+        saveWordHistory(text) {
+            //console.log(text);
+
+            this.saveWordHistoryCollection.push(text)
+            if (localStorage.getItem('wordHistory')) {
+                let wordArr = [...JSON.parse(localStorage.getItem('wordHistory')), ...this.saveWordHistoryCollection]
+                localStorage.setItem('wordHistory', JSON.stringify(_.uniq(wordArr)))
+            } else {
+                localStorage.setItem('wordHistory', JSON.stringify([]))
+            }
+
+
+        },
+        matchArticleToArray(text = '', subtitle = 0) {
+
+            return text.trim().match(/(\S.+?[.!?:;])(?=(\s)|$)(?<!\s([A-Z]|[a-z]){0,-1}.)/g)
+        },
+        replaceSidebarAtoZ(text) {
+            return text.trim().replace(/([^a-z]+)/gi, '')
+        },
+        banWords(text) {
+            text = text.toLowerCase();
+            return ['and', 'that', 'one', 'any', 'for', 'this', 'the'].includes(text) || text.length < 4
+        },
+        sidebarOneToArray(text) {
+            return _.uniq(text.toLowerCase().replace(/([^a-z]+)/gi, ' ').split(' ')).sort()
+        },
+        rawDataClean(text) {
+            //return unescape(text.replace(/([^a-z0-9.,'?!;]+)/gi, ' '))
+            return unescape(text.replace(/([^a-z0-9.,'?!;:şçüğıÇÜĞŞİĞÖö’]+)/gim, ' '))
+        },
+        encode: function(str) {
+            var buf = [];
+
+            for (var i = str.length - 1; i >= 0; i--) {
+                buf.unshift(['&#', str[i].charCodeAt(), ';'].join(''));
+            }
+
+            return buf.join('');
+        },
+        decode: function(str) {
+            return str.replace(/&#(\d+);/g, function(match, dec) {
+                return String.fromCharCode(dec);
+            });
+        },
+        saveWordHistorySend(text) {
+
+            fetch(this.apiUrl + '/addFavorite?text=' + text.trim() + '&type=save&url=0')
+                .then((response) => {
+                    return response.json()
+                })
+                .then((data) => {
+                    // console.log(data)
+                    this.sidebarAddWordText = '';
+                });
+        },
+        getAllWords() {
+
+            fetch(this.apiUrl + '/all_words.json')
+                .then((response) => {
+                    return response.json()
+                })
+                .then((data) => {
+                    this.all_words = data
+                });
+        }
     },
-    textSelectorEvent(text) {
-
-
-      if (text.trim()) {
-        this.tableHelperWords = []
-
-  //this.showDictionary = false
-  //text = text.trim().replace(/([^a-z\s']+)/gi, '');
-  this.sidebarAddWordText = text
-  this.highlightText = text
-  this.searchWord(text)
-
-
-}
-//this.sidebarAddWordText = window.getSelection().toString()
-//this.doubleClick(window.getSelection().toString())
-},
-textSelector(e) {
-      e.preventDefault();
-
-      var text = window.getSelection().toString()
-      if (text.trim()) {
-        this.tableHelperWords = []
-
-  //this.showDictionary = false
-  //text = text.trim().replace(/([^a-z\s']+)/gi, '');
-  this.sidebarAddWordText = text
-  this.highlightText = text
-  this.searchWord(text)
-
-
-}
-//this.sidebarAddWordText = window.getSelection().toString()
-//this.doubleClick(window.getSelection().toString())
-},
-doubleClick(text) {
-  this.tableHelperWords = []
-  this.dictionaryData = []
-  //this.showDictionary = false
-  text = text.trim().replace(/([^a-z]+)/gi, '');
-  this.sidebarAddWordText = text
-  this.highlightText = text
-  this.searchWord(text)
-  /*fetch('http://sentences.test/api/dictionary.php?type=important&text='+text).then((response) => {
-    return response.json()
-  }).then((data) => {
-   // console.log(data);
-    this.dictionaryData = data
-  })*/
-
-
-},
-saveWordHistory(text){
-  //console.log(text);
-
-  this.saveWordHistoryCollection.push(text)
-  if (localStorage.getItem('wordHistory')) {
-    let wordArr = [...JSON.parse(localStorage.getItem('wordHistory')),...this.saveWordHistoryCollection]
-    localStorage.setItem('wordHistory', JSON.stringify(_.uniq(wordArr)))
-  } else {
-    localStorage.setItem('wordHistory', JSON.stringify([]))
-  }
-
-
-},
-matchArticleToArray(text = '', subtitle = 0){
-
-  return text.trim().match(/(\S.+?[.!?:;])(?=(\s)|$)(?<!\s([A-Z]|[a-z]){0,-1}.)/g)
-},
-replaceSidebarAtoZ(text){
-  return text.trim().replace(/([^a-z]+)/gi, '')
-},
-banWords(text){
-  text = text.toLowerCase();
-  return ['and','that','one','any','for','this','the'].includes(text) || text.length < 4
-},
-sidebarOneToArray(text){
-  return _.uniq(text.toLowerCase().replace(/([^a-z]+)/gi, ' ').split(' ')).sort()
-},
-rawDataClean(text){
-  //return unescape(text.replace(/([^a-z0-9.,'?!;]+)/gi, ' '))
-  return unescape(text.replace(/([^a-z0-9.,'?!;:şçüğıÇÜĞŞİĞÖö’]+)/gim, ' '))
-},
-encode : function(str) {
-  var buf = [];
-
-  for (var i=str.length-1;i>=0;i--) {
-    buf.unshift(['&#', str[i].charCodeAt(), ';'].join(''));
-  }
-
-  return buf.join('');
-},
-decode : function(str) {
-  return str.replace(/&#(\d+);/g, function(match, dec) {
-    return String.fromCharCode(dec);
-  });
-},
-saveWordHistorySend(text) {
-
-  fetch(this.apiUrl + '/addFavorite?text=' + text.trim() + '&type=save&url=0')
-  .then((response) => {
-    return response.json()
-  })
-  .then((data) => {
-   // console.log(data)
-   this.sidebarAddWordText = '';
- });
-},
-getAllWords() {
-
-  fetch(this.apiUrl + '/all_words.json')
-  .then((response) => {
-    return response.json()
-  })
-  .then((data) => {
-    this.all_words = data
-  });
-}
-},
-created(){
-  //this.getAllWords();
-}
+    created() {
+        //this.getAllWords();
+    }
 });
-
-
